6a27664ecf0a72074a0475219e8919bf4ae27e29
Yuki_Matsuo <cylkaroof@gmail.com>
2018-03-07 17:50:40 +0900

    Import UnitySampleCode8

---
+++ Chapter8_AppleCatch/Assets/BasketController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour {
+
+	public AudioClip appleSE;	//変数を宣言しただけなのでpublicして，実体を代入する必要がある
+	public AudioClip bombSE;
+	AudioSource aud;		//AudioSourceコンポーネントを使用する為の箱
+	GameObject director;
+
+	// Use this for initialization
+	void Start () {
+		this.director = GameObject.Find ("GameDirector");
+		this.aud = GetComponent<AudioSource> ();
+	}
+
+
+
+	void OnTriggerEnter(Collider other){	//OnTriggerEnterメソッド(変数型 変数名)．メソッドは呼び出された際に実行される
+		//衝突相手はOnTriggerEnterメソッドの引数として（変数otherに）渡される．ただし引数として渡されるのは，衝突相手のゲームオブジェクトではなく，ゲームオブジェクトに
+		//アタッチされたコライダとなる．
+		//衝突▶▶collider.appleが「OnTriggerEnterメソッド」の変数に代入される▶▶Destroyメソッドにも適用される
+		//？？Collider型の変数を宣言したことでColliderが，衝突時(OTEメソッド実行時)にother変数に，代入してくれるの？？
+		//衝突して初めてother.gameObject.tagを取得できるってこと？
+		if (other.gameObject.tag == "Apple") {
+			this.director.GetComponent<GameDirector> ().GetApple (); 
+			//this.director無くても実行されるんじゃね？▶▶this.directorにGameObjectの「GameDirector」を代入しており，その，GameDirectorが持つ
+			//コンポーネント「GameDirector(.cs)」を呼び出しているので，this.directorは必要
+			this.aud.PlayOneShot (this.appleSE);	//audioSourceコンポーネントの，PlayOneShotメソッドに，appleSEを代入
+		} else {
+			this.director.GetComponent<GameDirector> ().GetBomb ();
+			this.aud.PlayOneShot(this.bombSE);
+		}
+		Destroy (other.gameObject);
+			
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);//mainカメラからタップした座標までのベクトルを取得
+			//http://megumisoft.hatenablog.com/entry/2015/08/13/172136
+			//https://qiita.com/4_mio_11/items/4b10c6fe37fd7a856350
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {	//PhysicsのRaycastメソッド（引数(変数hit)）
+				float x = Mathf.RoundToInt (hit.point.x);			//MathfのRoundToIntメソッド（引数）
+				float z = Mathf.RoundToInt (hit.point.z);			//返された変数hitが持つ変数pointの中のz
+				//つまり，hitの中には，変数pointを使ってz座標を表すコードが書かれている
+				transform.position = new Vector3 (x, 0, z);
+			}
+
+		}	
+	}
+}
---
+++ Chapter8_AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+	GameObject timerText;
+	GameObject pointText;
+	float time = 30.0f;
+	int point = 0;
+	GameObject generator;
+
+	public void GetApple(){
+		this.point += 100;
+	}
+
+	public void GetBomb(){
+		this.point /= 2;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		this.generator = GameObject.Find ("ItemGenerator");
+		this.timerText = GameObject.Find ("Time");	//シーンビューからUI部品の実体を検索してtimerText変数に代入
+		this.pointText = GameObject.Find("Point");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		this.time -= Time.deltaTime;
+
+		if (this.time < 0) {
+			this.generator.GetComponent<ItemGenerator> ().SetParameter (10000.0f, 0, 0);
+		} else if (0 <= this.time && this.time < 5) {
+			this.generator.GetComponent<ItemGenerator> ().SetParameter (0.7f, -0.04f, 3);
+		} else if (5 <= this.time && this.time < 12) {
+			this.generator.GetComponent<ItemGenerator> ().SetParameter (0.5f, -0.05f, 6);
+		} else if (12 <= this.time && this.time < 23) {
+			this.generator.GetComponent<ItemGenerator> ().SetParameter (0.8f, -0.04f, 4);
+		} else if (23 <= this.time && this.time < 30) {
+			this.generator.GetComponent<ItemGenerator> ().SetParameter (1.0f, -0.03f, 2);
+		}
+
+		this.timerText.GetComponent<Text> ().text = this.time.ToString ("F1");
+		//ToStringメソッドの引数に「F1」(小数点以下第一位まで)の文字列(書式指定子)を指定．
+		this.pointText.GetComponent<Text>().text = this.point.ToString() + " point"; //文字列に変換(整数?)+ point
+	}
+}
